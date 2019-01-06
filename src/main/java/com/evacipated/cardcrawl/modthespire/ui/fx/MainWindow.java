d1b9d6781e814fe27fd98b355f0691388b478df4
MikeSchoolAccnt <purcell.14@wright.edu>
2019-01-06 15:25:51 -0500

    Finish styling bottom part. Switch to using @FXML annotations

--- src/main/java/com/evacipated/cardcrawl/modthespire/ui/fx/MainWindow.java
+++ src/main/java/com/evacipated/cardcrawl/modthespire/ui/fx/MainWindow.java
@@ -2,26 +2,38 @@ package com.evacipated.cardcrawl.modthespire.ui.fx;
 
 import com.evacipated.cardcrawl.modthespire.ui.fx.viewmodels.ViewModel;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class MainWindow extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindow extends Application implements Initializable {
 
     private ViewModel viewModel;
+    private DropShadow launchButtonDropShadow = new DropShadow(10, Color.BLACK);
 
     //UI Elements
-    private Button buttonLaunch;
-    private Button buttonWorkshop;
-    private Button buttonUpdates;
-    private AnchorPane modListPane;
-    private AnchorPane bottomPane;
-    private AnchorPane modInfoPane;
+    @FXML private Button buttonLaunch;
+    @FXML private Button buttonWorkshop;
+    @FXML private Button buttonUpdates;
+    @FXML private AnchorPane modListPane;
+    @FXML private AnchorPane bottomPane;
+    @FXML private AnchorPane modInfoPane;
+    @FXML private AnchorPane launchButtonPane;
+    @FXML private RadioButton debugButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -29,7 +41,6 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        viewModel = new ViewModel();
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("assets/fx/views/main.fxml"));
         Scene mainScene = new Scene(root);
@@ -37,18 +48,13 @@ public class MainWindow extends Application {
         primaryStage.setScene(mainScene);
         primaryStage.show();
 
-        initUIElements(mainScene);
-        initOnClicks();
-        initStyles();
     }
 
-    private void initUIElements(Scene scene){
-        buttonLaunch = (Button) scene.lookup("#buttonLaunch");
-        buttonWorkshop = (Button) scene.lookup("#buttonWorkshop");
-        buttonUpdates = (Button) scene.lookup("#buttonUpdates");
-        modListPane = (AnchorPane) scene.lookup("#leftPane");
-        modInfoPane = (AnchorPane) scene.lookup("#rightPane");
-        bottomPane = (AnchorPane) scene.lookup("#bottomPane");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        viewModel = new ViewModel();
+        initOnClicks();
+        initStyles();
     }
 
     private void initStyles(){
@@ -57,8 +63,16 @@ public class MainWindow extends Application {
 
     private void initOnClicks(){
         buttonLaunch.setOnMouseClicked(event -> {
-            //TODO check debug checkbox
-            viewModel.launchSTS(false);
+            Boolean selected = debugButton.isSelected();
+            viewModel.launchSTS(selected);
+        });
+
+        buttonLaunch.setOnMousePressed(event -> {
+            launchButtonPane.setEffect(new Blend());
+        });
+
+        buttonLaunch.setOnMouseReleased(event -> {
+            launchButtonPane.setEffect(launchButtonDropShadow);
         });
 
         buttonUpdates.setOnMouseClicked(event -> {
@@ -69,5 +83,6 @@ public class MainWindow extends Application {
             viewModel.openWorkshopUrl();
         });
     }
+
 
 }
